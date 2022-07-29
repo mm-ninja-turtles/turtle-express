70fc65888f8084695aec76c3c495c2ba1a210f42
wai-lin <wailin.wilbert@gmail.com>
2022-07-29 17:12:54 +0630

    chore(dist): 🍰 remove dist folder, improve gitignore file

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { createRouter } from './create-router';
-export type { Method } from './types';
-export type { UseFunction, SetupFunction, SetupOptions, CreateRouterReturnType, } from './create-router';
-export type { Handler, HandlerOptions, Path, PathFunction, PathOptions, PathReturnType, Resolver, } from './create-path';
-//# sourceMappingURL=index.d.ts.map
--- dist/types.d.ts
+++
@@ -1,3 +0,0 @@
-/** HTTP request Methods */
-export declare type Method = 'get' | 'post' | 'put' | 'delete' | 'patch';
-//# sourceMappingURL=types.d.ts.map
